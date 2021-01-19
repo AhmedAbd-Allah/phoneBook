8c088f2745ba34a42b41f5a6ee20bd94f831ff9e
AhmedAbd-Allah <ahmedabdallah.programming@gmail.com>
2021-01-19 02:12:55 +0200

    contact model

---
+++ Models/contact.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const { uuid } = require('uuidv4');
+
+const contactSchema = new Schema({
+    id: {
+        type: String,
+        unique: true,
+        default: uuid()
+    },
+    name: {
+        type: String,
+    },
+    phoneNumber: {
+        work: {
+            type: Number,
+        },
+        home: {
+            type: Number,
+        },
+        mobile: {
+            type: Number,
+        },
+        other: {
+            type: Number,
+        },
+    },
+    emailAddress: {
+        type: String,
+    },
+    mailingAddress: {
+        type: String,
+    }
+});
+
+
+mongoose.model('Contacts', contactSchema);
+module.exports = mongoose.model('Contacts');
