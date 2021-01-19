e9dc60306c314dd1c44b9abbce750090aeaef1c0
AhmedAbd-Allah <ahmedabdallah.programming@gmail.com>
2021-01-19 02:01:23 +0200

    connect to DB

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const express = require('express');
 const app = express();
+const Config = require('./config')
+const mongoose = require('mongoose')
+const dbURI = Config.mongoURI;
+
+// Start database connection
+mongoose.connect(dbURI,
+    { useNewUrlParser: true, useUnifiedTopology: true, 'useCreateIndex': true }
+    , function (err, data) {
+        if (err)
+            console.log(err)
+    });
+
+// allow cross origin
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
 
 // server up
 app.listen(Config.serverPort, () => {
